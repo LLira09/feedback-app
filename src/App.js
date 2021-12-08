73fed4973bb3fa8a22e1fde2a9d462973f61f1ae
Jake Lira <llira09@gmail.com>
2021-12-08 14:34:46 -0600

    added FeedbackItem, FeedbackData and some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
     return (
         <>
             <Header />
             <div className='container'>
-                <h1>My App</h1>
+                <FeedbackItem />
             </div>
         </>
     )
