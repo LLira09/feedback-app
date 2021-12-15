ab518dfda96fc42b30859bddd6e0f6f410638fc2
Jake Lira <llira09@gmail.com>
2021-12-15 14:52:22 -0600

    added react-router-dom v6 and fixed routes

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+function AboutPage() {
+  return (
+    <Card>
+      <div className='about'>
+        <h1>About Project</h1>
+        <p>This is a React app to leace feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
